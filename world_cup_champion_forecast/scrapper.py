f4fb613f9115099ecdff6bab4c082adad8e622c7
Celsa Diaz <celsadiaz@Celsas-Air.fritz.box>
2018-04-28 17:34:11 +0200

    add scrapper iterating over all countries

--- world_cup_champion_forecast/scrapper.py
+++ world_cup_champion_forecast/scrapper.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 import requests
 import re
 
-url = 'http://sofifa.com/players?offset=0'
+###### Parameters
+
+url_fifa_base = 'http://sofifa.com/players?'
 
 fifa_stats = ['Crossing', 'Finishing', 'Heading Accuracy',
               'Short Passing', 'Volleys', 'Dribbling', 'Curve',
@@ -14,6 +17,16 @@ fifa_stats = ['Crossing', 'Finishing', 'Heading Accuracy',
               'Sliding Tackle', 'GK Diving', 'GK Handling', 'GK Kicking',
               'GK Positioning', 'GK Reflexes']
 
+# nation IDs are between [1-251]
+nations = 251
+
+# the dates that we want to consider are 1 january of years 2018, 2014, 2010
+days_code = [158961, 157501, 156090]
+years_code = [18, 14, 10]
+
+# number of top players to consider
+n_players = 11
+
 
 def soup_maker(url):
     r = requests.get(url)
@@ -22,96 +35,80 @@ def soup_maker(url):
     return soup
 
 
-def find_top_players(soup):
-    final_details = {}
-    table = soup.find('table', {'class': 'table-striped'})
+def find_top_players(soup, n_players):
+    table = soup.find('table', {'class': 'table-hover'})
     tbody = table.find('tbody')
-    all_a = tbody.find_all('a', {'class': ''})
-    for player in all_a:
+    all_a = tbody.find_all('a', {'class': '', 'href': re.compile('player/')})
+    final_results = []
+    for player in all_a[:n_players]:
+        final_details = {}
         final_details['short_name'] = player.text
         final_details.update(player_all_details('http://sofifa.com' + player['href']))
         print(final_details)
+        final_results.append(final_details)
+    return final_results
 
 
 def find_player_info(soup):
     player_data = {}
     player_data['image'] = soup.find('img')['data-src']
     player_data['full_name'] = soup.find('h1').text.split(' (')[0]
-    span = soup.find('span', attrs={'class': None}).text.strip()
+    span = soup.find('span', attrs={'class': None}).contents[-1].strip()
+    player_data['nation'] = soup.find('span', attrs={'class': None}).contents[1].attrs['title']
     dob = re.search('(\(.*)\)', span).group(0)
     player_data['dob'] = dob.replace('(', '').replace(')', '')
     infos = span.replace(dob + ' ', '').split(' ')
-    player_data['pref_pos'] = infos[:infos.index('Age')]
     player_data['age'] = int(infos[infos.index('Age') + 1: -2][0])
-    player_data['height'] = int((infos[infos.index('Age') + 2: -1][0]).replace('cm', ''))
-    player_data['weight'] = int((infos[infos.index('Age') + 3:][0]).replace('kg', ''))
     return (player_data)
 
 
 def find_player_stats(soup):
     player_data = {}
-    info = re.findall('\d+', soup.text)
-    player_data['rating'] = int(info[0])
-    player_data['potential'] = int(info[1])
-    player_data['value'] = int(info[2])
-    player_data['wage'] = int(info[3])
-    return (player_data)
-
-
-def find_player_secondary_info(soup):
-    player_data = {}
-    player_data['preff_foot'] = soup.find('label', text='Preferred Foot') \
-        .parent.contents[2].strip('\n ')
-    player_data['club'] = soup.find_all('ul')[1].find('a').text
-    player_data['club_pos'] = soup.find('label', text='Position') \
-        .parent.find('span').text
-    player_data['club_jersey'] = soup.find('label', text='Jersey number') \
-        .parent.contents[2].strip('\n ')
-    if soup.find('label', text='Joined'):
-        player_data['club_joined'] = soup.find('label', text='Joined') \
-            .parent.contents[2].strip('\n ')
-    player_data['contract_valid'] = soup.find(
-        'label', text='Contract valid until') \
-        .parent.contents[2].strip('\n ')
-    if len(soup.find_all('ul')) > 2:
-        player_data['country'] = soup.find_all('ul')[2].find('a').text
-    return (player_data)
-
-
-def find_fifa_info(soup):
-    player_data = {}
-    divs_without_skill = soup[1].find_all('div', {'class': 'col-3'})[:3]
-    more_lis = [div.find_all('li') for div in divs_without_skill]
-    lis = soup[0].find_all('li') + more_lis[0]
-    for li in lis:
-        for stats in fifa_stats:
-            if stats in li.text:
-                player_data[stats.replace(' ', '_').lower()] = int(
-                    (li.text.split(' ')[0]).replace('\n', ''))
-    traits = soup[1].find('h4', text='Traits')
-    if traits:
-        player_data['traits'] = [li.text.replace('\xa0', '') for li in
-                                 traits.parent.next_sibling.next_sibling.find_all('li')]
-    specialities = soup[1].find('h4', text='Specialities')
-    if specialities:
-        player_data['specialities'] = [li.text.replace('\xa0', '') for li in
-                                       specialities.parent.next_sibling.next_sibling.find_all('li')]
+    info = re.findall('\d+\.?\d?', soup.text)
+    player_data['rating'] = float(info[0])
+    player_data['potential'] = float(info[1])
+    player_data['value'] = float(info[2])
+    player_data['wage'] = float(info[3])
     return (player_data)
 
 
 def player_all_details(url):
     all_details = {}
+    print(url)
     soup = soup_maker(url)
     player_info = soup.find('div', {'class': 'player'})
     all_details.update(find_player_info(player_info))
     player_stats = soup.find('div', {'class': 'stats'})
     all_details.update(find_player_stats(player_stats))
-    secondary_info = soup.find('div', {'class': 'teams'})
-    all_details.update(find_player_secondary_info(secondary_info))
-    fifa_info = soup.find_all('div', {'class': 'columns mb-20'})
-    all_details.update(find_fifa_info(fifa_info))
-    return (all_details)
+    return all_details
 
 
-soup = soup_maker(url)
-find_top_players(soup)
+def get_all_players_statistics_per_country_per_year(url_fifa_base, country, year, day, n_players):
+    url = "{url_fifa_base}na%5B0%5D={country}&col=vl&sort=desc&v={year}&e={day}&set=true".format(
+        url_fifa_base=url_fifa_base,
+        country=country,
+        year=year,
+        day=day
+    )
+    print(url)
+
+    soup = soup_maker(url)
+    results_per_country_per_year = find_top_players(soup, n_players)
+    results_per_country_per_year = pd.DataFrame(results_per_country_per_year)
+    results_per_country_per_year['year'] = year
+    results_per_country_per_year['country_code'] = country
+    return results_per_country_per_year
+
+
+def query_all_fifa_players_info(nations, years_code, days_code, n_players):
+    results_all = []
+    for nation in range(1, nations):
+        for i in range(len(years_code)):
+            results = get_all_players_statistics_per_country_per_year(url_fifa_base, nation, years_code[i],
+                                                                      days_code[i], n_players)
+            results_all.append(results)
+
+    pd.concat(results_all).to_csv('./data/results.csv', index=False, encoding='utf-8')
+
+
+query_all_fifa_players_info(nations, years_code, days_code, n_players)
