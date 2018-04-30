0d2f21f70e438809e3a529c87905145bfe0b7c96
Celsa Diaz <celsadiaz@Celsas-Air.fritz.box>
2018-04-30 11:05:06 +0200

    update correct fifa_stats

--- world_cup_champion_forecast/scrapper.py
+++ world_cup_champion_forecast/scrapper.py
@@ -1,8 +1,9 @@
-from bs4 import BeautifulSoup as bs
-import pandas as pd
-import requests
 import re
 import time
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
 
 ###### Parameters
 
@@ -55,8 +56,8 @@ def find_player_info(soup):
     player_data['image'] = soup.find('img')['data-src']
     player_data['full_name'] = soup.find('h1').text.split(' (')[0]
     span = soup.find('span', attrs={'class': None}).contents[-1].strip()
-    player_data['nation'] = soup.find('span', attrs={'class': None}).contents[1].attrs['title']
-    dob = re.search('(\(.*)\)', span).group(0)
+    player_data['nation'] = soup.find('span', attrs={'class': None}).contents[1]['title']
+    dob = re.search('\(.*\)', span).group(0)
     player_data['dob'] = dob.replace('(', '').replace(')', '')
     infos = span.replace(dob + ' ', '').split(' ')
     player_data['age'] = int(infos[infos.index('Age') + 1: -2][0])
@@ -107,14 +108,15 @@ def query_all_fifa_players_info(nations, years_code, days_code, n_players):
         for i in range(len(years_code)):
             try:
                 results = get_all_players_statistics_per_country_per_year(url_fifa_base, nation, years_code[i],
-                                                                      days_code[i], n_players)
+                                                                          days_code[i], n_players)
                 results_all.append(results)
             except:
                 print("it failed to get {} from year {} from nation {}.".format(i, years_code[i], nation))
 
         time.sleep(60)
 
-    pd.concat(results_all).to_csv('./world_cup_champion_forecast/data/fifa_players_data.csv', index=False, encoding='utf-8')
+    pd.concat(results_all).to_csv('./world_cup_champion_forecast/fifa_stats.csv', index=False,
+                                  encoding='utf-8')
 
 
 query_all_fifa_players_info(nations, years_code, days_code, n_players)
