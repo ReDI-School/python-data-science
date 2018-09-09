4f781cff74535c8481d7e64d13ef37cf2361c814
LBrinkmann <com@levin-brinkmann.de>
2018-09-09 18:24:05 +0200

    Add ws18 package.

---
+++ ws_18/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+# configure these
+PACKAGE_NAME = 'redi-datascience-ws18'
+MODULE_NAME = 'redi'
+VERSION = '0.0.5'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+            'matplotlib',
+            'seaborn',
+            'numpy',
+            'pandas',
+            'ipdb'
+        ],
+
+    extras_require={
+        'dev': []
+    },
+
+    cmdclass={
+    },
+
+    scripts=[
+    ],
+
+    # run `python3 setup.py prepare_build`
+    # followed by `python3 setup.py bdist`
+    # then you can check the `build` folder to see if your package data is included in
+    # the binary distribution
+    package_data={
+        '': [
+            'resources/*',
+            'config/*',
+            'datasets/*'
+        ]
+    }
+)
